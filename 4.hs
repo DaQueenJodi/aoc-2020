6f0854243c4a3910cd20a5c0126ce6bdf8a77931
JodiMcJodson <isekgood@gmail.com>
2023-07-25 18:34:16 -0500

    4

--- 4.hs
+++ 4.hs
@@ -1,7 +1,9 @@
 import Debug.Trace (trace)
 import Data.List (findIndex)
-import Data.Maybe (fromJust)
-import Data.Char (isSpace)
+import Data.Maybe ( fromJust, isJust)
+import Data.Char (isSpace, isNumber)
+
+import Text.Read (readMaybe)
 
 
 main :: IO ()
@@ -17,25 +19,49 @@ split f xs
   where
     (p1, _:p2) = splitAt idx xs
     idx = fromJust (f `findIndex` xs)
-type Passport = [String]
+type Passport = [(String, String)]
+
+getVal :: String -> [(String, a)] -> Maybe a
+getVal _ [] = Nothing
+getVal s (x:xs) = if k == s then Just v else getVal s xs
+  where
+    (k, v) = x
+
+
+
+passportChecks :: [(String, String -> Bool)]
+passportChecks =
+  [ ("byr", \x -> let xInt = readMaybe x in
+     length x == 4 && isJust xInt && fromJust xInt >= 1920 && fromJust xInt <= 2002)
+  , ("iyr", \x -> let xInt = readMaybe x in
+     length x == 4 && isJust xInt && fromJust xInt >= 2010 && fromJust xInt <= 2020)
+  , ("eyr", \x -> let xInt = readMaybe x in
+     length x == 4 && isJust xInt && fromJust xInt >= 2020 && fromJust xInt <= 2030)
+  , ("hgt", \x -> let xInt = readMaybe (takeWhile isNumber x)
+                      unit = dropWhile isNumber x
+                  in isJust xInt && case unit of
+                      "cm" -> fromJust xInt >= 150 && fromJust xInt <= 193
+                      "in" -> fromJust xInt >= 59 && fromJust xInt <= 76
+                      _    -> False)
+  , ("hcl", \x -> length x == 7 && all (\ x -> isNumber x || (x <= 'f' && x >= 'a')) (tail x))
+  , ("ecl", \x -> x `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
+  , ("pid", \x -> length x == 9 && all isNumber x)
+  ]
 
 solve :: String -> Int
 solve s = length $ filter valid (passports s)
   where
-    valid passport = all ((== True) . (`elem` passport))
-      [ "byr"
-      , "iyr"
-      , "eyr"
-      , "hgt"
-      , "hcl"
-      , "ecl"
-      , "pid"
-      ]
-    passports :: String -> [Passport]
-    passports s = ps'
+    valid :: Passport -> Bool
+    valid passport = trace (show passport ++ "\n" ++ show res) res
       where
-        ps :: [Passport]
-        ps' = map (map (takeWhile (/= ':'))) ps
-        ps = trace (show (map (split isSpace) chunks)) (map (split isSpace) chunks)
+        res = all (\ (k, f) -> let maybeV = getVal k passport
+                                     in isJust maybeV && f (fromJust maybeV)) passportChecks
+
+    passports :: String -> [Passport]
+    passports s = map mkPassport psRaw
+      where
+        psRaw = map (split isSpace) chunks
         chunks = map unwords $ split null ls
         ls = lines s
+    mkPassport :: [String] -> Passport
+    mkPassport = map (\ x -> let [key, val] = split (== ':') x in (key, val))
